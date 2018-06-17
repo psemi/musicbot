fe94d8e3eb08e52def10b6a15f6b37c9c6884073
psemi <40316342+psemi@users.noreply.github.com>
2018-06-17 11:21:26 +0300

    Delete gulpfile.js

--- schedulebot-heroku/gulpfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var path = require('path');
-var gulp = require('gulp');
-var nsp = require('gulp-nsp');
-var plumber = require('gulp-plumber');
-
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
